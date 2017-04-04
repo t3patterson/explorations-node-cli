1a14a5bd6a52ad91c4b60559dcd97be747d3f5f6
Travis Hubbard <t3patterson@gmail.com>
2017-04-03 22:41:08 -0400

    adding inquirer module to prompt user

--- broadcast.js
+++ broadcast.js
@@ -1,6 +1,10 @@
-const fs = require('fs')
-const {parse} = require('csv')
-const program = require('commander')
+const fs = require('fs');
+const {parse} = require('csv');
+const program = require('commander');
+const inquirer = require('inquirer');
+
+
+
 
 program
 	.version('0.0.1')
@@ -10,9 +14,21 @@ program
 let stream = fs.createReadStream(program.list)
   .pipe(parse({ delimter: ',' }));
 
-stream.on('data', (data)=>{
-	let firstName = data[0];
-	let lastName = data[1];
-	let email = data[2];
-	console.log(firstName, lastName, email)
-})
+stream
+	.on('data', (dataRowArray)=>{
+		// console.log(dataRowArray)//csv row as an Array
+		let firstName = dataRowArray[0];
+		let lastName = dataRowArray[1];
+		let email = dataRowArray[2];
+		// console.log(firstName, lastName, email)
+	})
+	.on("end", ()=>{
+		let questions = [
+			{ type: 'input', name: 'sender.email', message: "Sender's email address > " },
+ 			{ type: 'input', name: 'sender.name', message: "Sender's name > " },
+			{ type: 'input', name: 'subject', message: "Subject > " },
+		]
+		inquirer.prompt(questions).then((answers)=>{
+			console.log(answers);
+		})
+	})
