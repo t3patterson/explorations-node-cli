f36cf1a7ac7e97e887e4274f17b050fe7fbe98a8
Travis Hubbard <t3patterson@gmail.com>
2017-04-03 21:41:24 -0400

    demoing commander

---
+++ broadcast.js
@@ -0,0 +1,8 @@
+const program = require('commander')
+
+program
+	.version('0.0.1')
+   .option('-l, --list [list]', 'list of customers in CSV file')
+	.parse(process.argv)
+	
+console.log(program.list)
