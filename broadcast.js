73234f962aa533ae11b5a691218c68e35f020e06
Travis Hubbard <t3patterson@gmail.com>
2017-04-03 22:29:42 -0400

    including parser and fs.readstream

--- broadcast.js
+++ broadcast.js
@@ -1,8 +1,18 @@
+const fs = require('fs')
+const {parse} = require('csv')
 const program = require('commander')
 
 program
 	.version('0.0.1')
-   .option('-l, --list [list]', 'list of customers in CSV file')
+   .option('-l, --list [csv_list]', 'list of customers in CSV file')
 	.parse(process.argv)
 	
-console.log(program.list)
+let stream = fs.createReadStream(program.list)
+  .pipe(parse({ delimter: ',' }));
+
+stream.on('data', (data)=>{
+	let firstName = data[0];
+	let lastName = data[1];
+	let email = data[2];
+	console.log(firstName, lastName, email)
+})
